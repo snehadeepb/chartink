e371052f98cf79633945934ceda2d848ff83dff8
Snehadeep  Barwade <115915017+snehadeepb@users.noreply.github.com>
2024-10-15 18:14:49 +0530

    Add files via upload

---
+++ chartink/chart.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+class chartink():
+  def __init__(self , condition):
+    self.condition = condition
+  def stock_screener(self):
+
+    with requests.Session() as s:
+        r = s.get('https://chartink.com/screener/time-pass-48')
+        soup = bs(r.content, 'lxml')
+        s.headers['X-CSRF-TOKEN'] = soup.select_one('[name=csrf-token]')['content']
+        r = s.post('https://chartink.com/screener/process', data=self.condition).json()
+        #print(r.json())
+        df = pd.DataFrame(r['data'])
+        return df
